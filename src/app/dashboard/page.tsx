0160166ff98abb1b7cba2100787aebf8faeac8c5
Muhammad Umair <umaair40@gmail.com>
2025-11-03 15:03:10 +0500

    Dashboard Page Added

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,20 @@
 import Navbar from '@/components/Navbar'
 import React from 'react'
+import WelcomeSection from '@/components/dashboard/WelcomeSection'
+import MainActions from '@/components/dashboard/MainActions'
+import ActivityOverview from '@/components/dashboard/ActivityOverview'
 
 function Dashboard() {
   return (
-    <div>
+    <>
       <Navbar/>
-      Dashboard
-    </div>
+      <div className='max-w-7xl mx-auto px-6 py-8 pt-24'>
+        <WelcomeSection/>
+        <MainActions/>
+        <ActivityOverview/>
+      </div>
+      
+    </>
   )
 }
 
